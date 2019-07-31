9c7eec31fb78e50db8f732ea8929c9a677d9d1a1
Laura Girdwood <azevedoemello@gmail.com>
2019-07-31 08:19:25 -0300

    subindo referência de estrutura security + swagger

---
+++ back-end/back-end/src/main/java/org/generation/brazil/backend/user/User.java
@@ -0,0 +1,64 @@
+package org.generation.brazil.backend.user;
+
+import java.util.HashSet;
+import java.util.Set;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.generation.brazil.backend.role.Role;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "user", uniqueConstraints = {
+    @UniqueConstraint(columnNames = {
+        "username"
+    }),
+    @UniqueConstraint(columnNames = {
+        "email"
+    })
+})
+public class User {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @NotBlank
+  @Size(max = 40)
+  private String name;
+
+  @NotBlank
+  @Size(max = 15)
+  private String username;
+
+  @Email
+  @NotBlank
+  @Size(max = 40)
+  private String email;
+
+  @NotBlank
+  @Size(max = 100)
+  private String password;
+
+  @ManyToMany(fetch = FetchType.LAZY)
+  @JoinTable(name = "user_role",
+      joinColumns = @JoinColumn(name = "user_id"),
+      inverseJoinColumns = @JoinColumn(name = "role_id"))
+  private Set<Role> roles = new HashSet<>();
+
+}
