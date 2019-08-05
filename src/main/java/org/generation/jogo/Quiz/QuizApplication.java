3af1b945de719a15bcc5bb8c2922996018a16fad
Laura Girdwood <azevedoemello@gmail.com>
2019-08-05 07:22:17 -0300

    add componentScan

--- src/main/java/org/generation/jogo/Quiz/QuizApplication.java
+++ src/main/java/org/generation/jogo/Quiz/QuizApplication.java
@@ -4,9 +4,11 @@ import org.generation.jogo.Quiz.jogador.Jogador;
 import org.generation.jogo.Quiz.jogador.JogadorController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 
 @SpringBootApplication
+@ComponentScan(basePackages = "org.generation.jogo.Quiz.config")
 public class QuizApplication {
 
 	public static void main(String[] args) {
